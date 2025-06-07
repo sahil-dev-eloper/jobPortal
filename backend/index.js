9cc73e2157308e67be9b74e93a252a3c31d66213
sahil-dev-eloper <sahildargar@gmail.com>
2025-06-07 13:02:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 // Configuring CORS
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://jobportal-frontend-43l5.onrender.com',
     credentials: true
 }
 
