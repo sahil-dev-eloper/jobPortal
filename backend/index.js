a86cf64058774d5cd7753652953cec9231376bb8
sahil-dev-eloper <sahildargar@gmail.com>
2025-06-30 17:54:17 +0530

    changes done again

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 // Configuring CORS
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ["http://localhost:5173", "https://job-portal-ten-rho.vercel.app"],
     credentials: true
 }
 app.use(cors(corsOptions));
